4e13bf9623a659805c6a05a3e8a960996bbaac2c
HUN1i <nicebrian0328@gmail.com>
2023-10-11 19:57:37 +0900

    ADD :: AI 명령

--- controller/aiController.js
+++ controller/aiController.js
@@ -4,10 +4,17 @@ const router = express.Router();
 
 const aiService = new AiService();
 
-router.post("/ai", async (req, res) => {
+router.post("/story", async (req, res) => {
   const DTO = req.body;
-  console.log(DTO);
-  const result = await aiService.getGPT(DTO);
+  const result = await aiService.getStory(DTO);
+
+  return res.status(200).send(result);
+});
+
+router.post("/choice", async (req, res) => {
+  const DTO = req.body;
+  const result = await aiService.getChoice(DTO);
+
   return res.status(200).send(result);
 });
 module.exports = router;
--- services/aiService.js
+++ services/aiService.js
@@ -1,17 +1,38 @@
 require("dotenv").config();
 const OpenAI = require("openai");
-
+const openai = new OpenAI({
+  apiKey: process.env.AI_APIKEY,
+});
 class AiService {
-  async getGPT(DTO) {
+  async getStory(DTO) {
     try {
-      console.log(DTO.question);
-      const openai = new OpenAI({
-        apiKey: process.env.AI_APIKEY,
-      });
-
       const response = await openai.chat.completions.create({
         model: "gpt-4",
-        messages: [{ role: "user", content: `${DTO.question}` }],
+        messages: [
+          {
+            role: "user",
+            content: `"${DTO.question}" 이 다음에 이어질 스토리를 100자 이하로 적어 줘.`,
+          },
+        ],
+      });
+      return response.choices[0].message.content;
+    } catch (err) {
+      console.log(err);
+      return 500;
+    }
+  }
+
+  async getChoice(DTO) {
+    const { question, count } = DTO;
+    try {
+      const response = await openai.chat.completions.create({
+        model: "gpt-4",
+        messages: [
+          {
+            role: "user",
+            content: `"${question}" 이것과 연관지어서 선택지를 ${count} 개 적어 줘.`,
+          },
+        ],
       });
       return response.choices[0].message.content;
     } catch (err) {
